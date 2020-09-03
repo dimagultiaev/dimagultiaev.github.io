1e8c5dd1b866391d98fc4ca259a7e1d1ce47f9e7
DimaGyltiaev <52207685+DimaGyltiaev@users.noreply.github.com>
2020-09-04 00:55:17 +0300

    Work6 v3

---
+++ work6/js/script.js
@@ -0,0 +1,9 @@
+function message() {
+    var nokode = document.getElementById("nokode");
+
+    if (nokode.style.display === "none") {
+        nokode.style.display = "block";
+    } else {
+        nokode.style.display = "block";
+    }
+}
